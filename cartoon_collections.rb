f1937344a12eefce27c00982a14ae197d958dead
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-07-02 21:57:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   
-  array.each do
+  
 
   
 end
@@ -13,6 +13,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each do
+  
   
 end
